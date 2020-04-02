a61e5dacccb842cbc4f42c13bbd186c02a6cca7e
sujithakumar <sujithakumars@gmail.com>
2020-04-02 02:09:14 -0400

    deployables

--- src/app/Services/form-fields.service.ts
+++ src/app/Services/form-fields.service.ts
@@ -8,15 +8,20 @@ import { Observable, BehaviorSubject } from 'rxjs';
 })
 export class FormFieldsService {
 
+  url1 = "../../assets/Data/formFields.json";
+  url2 = "http://e4yl1.mocklab.io/formFields"
+  url = this.url1 ? this.url1 : this.url2;
+
   public ItemFields: BehaviorSubject<any> = new BehaviorSubject<any>({
   });
 
   constructor(private http: HttpClient) {
   }
 
+
   //get form details
   public getFormFields(): Observable<any> {
-    return this.http.get("../../assets/Data/formFields.json");
+    return this.http.get(this.url);
   }
 
   //passing data between components:
